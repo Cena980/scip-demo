940303047d98596deed333c95865b140cd6f8e35
Alisina Nazari <cena.nazari980@gmail.com>
2025-07-24 20:25:26 +0430

    Fixed embedded repo in client directory

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API Routes
+app.use('/api', apiRoutes);
+
+// Serve static files from React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+module.exports = app;
